793dfeec4a48c10221ff609f63a082409e511681
chukui <mark@MarkdeMacBook-Pro.local>
2016-06-22 18:39:36 +0800

    no message

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -16,7 +16,7 @@ return array(
     'USER_AUTH_MODEL'=>'user',  // 默认验证数据表模型
     'AUTH_PWD_ENCODER'=>'md5',  // 用户认证密码加密方式
     'USER_AUTH_GATEWAY'=>'/Public/login',   // 默认认证网关
-    'NOT_AUTH_MODULE'=>'Public',        // 默认无需认证模块
+    'NOT_AUTH_MODULE'=>'Public,Baidu',        // 默认无需认证模块
     'REQUIRE_AUTH_MODULE'=>'',      // 默认需要认证模块
     'NOT_AUTH_ACTION'=>'',      // 默认无需认证操作
     'REQUIRE_AUTH_ACTION'=>'',      // 默认需要认证操作
