fe3e327ecb8df9e604fb121e0fc743d2d153ff33
chukui <mark@MarkdeMBP.lan>
2017-03-14 23:15:06 +0800

    优化编辑器

--- Public/own-js/Article/ae.js
+++ Public/own-js/Article/ae.js
@@ -7,86 +7,12 @@
         },
         currentTable: null,
         bindEvent: function () {
-            var that = this;    
-            //绑定新增文章点击事件
-            //var ue = UE.getEditor('container');
+            var that = this;
             $('select').select2();
-            $('#addArticleTag').modal({show:false});
-            $('#addArticleCategory').modal({show:false});
-            $('#alert-modal').modal({show:false});
-            $('#addOneTag').click(function(){
-                 $('#addArticleTag').modal('show');
-            });
-            $('#addOneCategory').click(function(){
-                 $('#addArticleCategory').modal('show');
-            });
-            $('#addTagForArticle').click(function(){
-                var displayname,displayorder;
-                //如果select2中的值不为空,获取select2中的data中
-                if( $('#addArticleTag input[name=displayname]').val() != ''){
-                    displayname = $('#addArticleTag input[name=displayname]').val();
-                }
-                if( $('#addArticleTag input[name=displayorder]').val() != ''){
-                    displayorder = $('#addArticleTag input[name=displayorder]').val();
-                }
-                //ajax 前端与后台沟通参数
-                var url = "/Tag/insert";
-                var sendData = { displayname: displayname, displayorder: displayorder};
-                //console.log(sendData);
-                $.ajax({
-                    url: url,
-                    data: sendData,
-                    dataType:'json',
-                    type: 'POST',
-                    success: function (data) {
-                        $("#addArticleTag").modal('hide');
-                        $('#alert-modal .alert-data-title').html('添加成功!');
-                        $('#alert-modal').modal();
-                        $('#tag_multi_select2').append('<option value="'+data.id+'" selected="selected" >'+data.displayname+'</option>').trigger('change');
-                    }
-                });
-            });
-            $('#addCategoryForArticle').click(function(){
-                var displayname,parentcategoryid,displayorder;
-                //如果select2中的值不为空,获取select2中的data中
-                if( $('#addArticleCategory input[name=displayname]').val() != ''){
-                    displayname = $('#addArticleCategory input[name=displayname]').val();
-                }
-                if( $('#addArticleCategory input[name=parentcategoryid]').val() != ''){
-                    parentcategoryid = $('#addArticleCategory input[name=parentcategoryid]').val();
-                }
-                if( $('#addArticleCategory input[name=displayorder]').val() != ''){
-                    displayorder = $('#addArticleCategory input[name=displayorder]').val();
-                }
-                //ajax 前端与后台沟通参数
-                var url = "/Category/insert";
-                var sendData = { displayname: displayname, parentcategoryid: parentcategoryid, displayorder: displayorder};
-                //console.log(sendData);
-                $.ajax({
-                    url: url,
-                    data: sendData,
-                    dataType: 'json',
-                    type: 'POST',
-                    success: function (data) {
-                        $("#addArticleCategory").modal('hide');
-                        $('#alert-modal .alert-data-title').html('添加成功!');
-                        $('#alert-modal').modal();
-                        if(data.parentcategoryid != ''){
-
-                        }else{
-                            $('#selOY2').append('<optgroup label="'+data.displayname+'" data-id="'+data.id+'"><option value="'+data.id+'" selected="selected" >'+data.displayname+'</option></<optgroup>').trigger('change');
-                        }
-                    }
-                });
-            });
-            $('input[name=imgFile]').change(function(){
-                previewImage(this);
-                $('#imghead').removeClass('hidden').show();
-            });
             $('input[name=title]').blur(function(){
                 var title = $(this).val();
                 if (title != '') {
-                    url = "/Article/checkTitleDuplicated/";
+                    url = "/article/checkTitleDuplicated.html";
                     sendData = {title:title};
                     $.ajax({
                         url : url,
@@ -102,22 +28,28 @@
                 }
             });
             $('input[name=imageFile]').fileupload({
-                url: '/article/uploadImage.html',
+                url: '/article/uploadImage.html?name=image',
                 dataType: 'json',
                 done: function (e, data) {
                     $('#imagePreview').empty().append('<img src="'+data.result.file_path+'" />');
                     $('input[name=image]').val(data.result.file_path);
                 }
-            }).prop('disabled', !$.support.fileInput)
-                .parent().addClass($.support.fileInput ? undefined : 'disabled');
+            });
+            var imageCount = 0
             $('#fileupload').fileupload({
-                url: '/article/uploadImage.html',
+                url: '/article/uploadImage.html?name=markdown',
                 dataType: 'json',
                 done: function (e, data) {
-                    $('#files').append('<p>'+data.result.file_path+ '</p>');
+                    var text = '';
+                    if(imageCount != 0)
+                    {
+                        text += "\n";
+                    }
+                    text +='![image]('+data.result.file_path+')';
+                    $('#article-content').append(text);
+                    imageCount++;
                 }
-            }).prop('disabled', !$.support.fileInput)
-                .parent().addClass($.support.fileInput ? undefined : 'disabled');
+            });
             $('.view-content').click(function () {
                 var content = $('#article-content').val();
                 if(content != '' || content != undefined)
